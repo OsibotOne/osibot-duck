67fbfda664d40708e84bbb74980f3b4b44dc8754
Adam-Osibot <74332472+Adam-Osibot@users.noreply.github.com>
2023-08-25 14:22:16 +1000

    SFTP Test Server Info Added

--- osibot-duck/rpi-duck/scripts/upload.sh
+++ osibot-duck/rpi-duck/scripts/upload.sh
@@ -3,7 +3,20 @@
 # Main Upload Script
 
 # Connect to AWS-Server via SFTP
-# Download doit.now file.
-# Upload now.dat to server
-# Upload cam1-temp.jpeg to server
+# Download doit.now file. /command/doit.now
+# Upload now.dat to server /data/now.dat
+# Upload cam1-temp.jpeg to server /pics/cam1-temp.jpeg
+
+
+
+
+#Test SFTP Server to test with:
+
+#username: acc764515107
+#password: Nemo2023!
+#server: home235874866.1and1-data.host
+#port: 22
+#protocol: SFTP
+
+
 
