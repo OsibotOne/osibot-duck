3380c455b1b890860d0f30de38f67eda9e6a89b1
Adam-Osibot <74332472+Adam-Osibot@users.noreply.github.com>
2023-09-06 10:05:22 +1000

    goto script

---
+++ osibot-duck/rpi-duck/scripts/goto.sh
@@ -0,0 +1,16 @@
+# Osibot-Duck
+
+# Structure  goto.sh  LATTITUDE, LONGITUDE
+# Example   goto.sh 12.477774747474, -147.3737747444
+
+# Connects to Autopilot via MAVLINK protocol (USB Serial)
+# Uploads new waypoint from script arguments above
+# Commands autopilot into AUTO mode to goto waypoint.
+# Set autopilot to armed status
+
+# Outputs success or any errors to vessel.log file
+
+# https://mavlink.io/en/mavgen_python/
+# https://github.com/ArduPilot/pymavlink
+# https://www.ardusub.com/developers/pymavlink.html#armdisarm-the-vehicle
+
